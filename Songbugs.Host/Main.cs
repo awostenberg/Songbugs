b7d88ed1865b7f35c62841b49473fd2e94e2702c
J. Wostenberg <awosty@gmail.com>
2012-12-15 16:28:57 -0700

    Created host project

---
+++ Songbugs.Host/Main.cs
@@ -0,0 +1,38 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+
+namespace Songbugs.Host {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args) {
+            NSApplication.Init();
+      
+            using (var p = new NSAutoreleasePool ()) {
+                NSApplication.SharedApplication.Delegate = new AppDelegate();
+                NSApplication.Main(args);
+            }
+        }
+    }
+
+    class AppDelegate : NSApplicationDelegate {
+        SBGame game;
+
+        public override void FinishedLaunching(MonoMac.Foundation.NSObject notification) {
+            game = new SBGame();
+            game.Run();
+        }
+    
+        public override bool ApplicationShouldTerminateAfterLastWindowClosed(NSApplication sender) {
+            return true;
+        }
+    }  
+}
+
+
