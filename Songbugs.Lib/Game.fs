2c132130618d465ee2153d54c0ef4b31299a7ca6
J. Wostenberg <>
2012-12-15 19:02:23 -0700

    Changed all definitions and calls to a more traditional F# style (mostly just by adding spaces)

--- Songbugs.Lib/Game.fs
+++ Songbugs.Lib/Game.fs
@@ -1,17 +1,17 @@
 namespace Songbugs.Lib
 open Microsoft.Xna.Framework
 
-type Game() as this =
-  inherit Microsoft.Xna.Framework.Game()
+type Game () as this =
+  inherit Microsoft.Xna.Framework.Game ()
   
-  member val Graphics = new GraphicsDeviceManager(this) with get, set
+  member val Graphics = new GraphicsDeviceManager (this) with get, set
   
-  override this.Initialize() =
+  override this.Initialize () =
     this.Content.RootDirectory <- "../Resources"
     this.Graphics.IsFullScreen <- false
     this.IsMouseVisible <- true
     
-    base.Initialize()
+    base.Initialize ()
   
-  override this.Draw(_) =
-    this.GraphicsDevice.Clear(Color.Black)
+  override this.Draw _ =
+    this.GraphicsDevice.Clear (Color.Black)
