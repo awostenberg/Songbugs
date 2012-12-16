531ebd5cc35a6a73fc63537bc4e2d49a6f34d8e4
J. Wostenberg <>
2012-12-15 19:05:06 -0700

    Created an IGameObject interface

---
+++ Songbugs.Lib/GameObject.fs
@@ -0,0 +1,12 @@
+namespace Songbugs.Lib
+open Microsoft.Xna.Framework
+
+type IGameObject =
+  
+  abstract member Initialize : unit -> unit default this.Initialize() = ()
+  
+  abstract member LoadContent : unit -> unit default this.LoadContent() = ()
+  
+  abstract member Update : GameTime -> unit default this.Update _ = ()
+  
+  abstract member Draw : GameTime -> unit default this.Draw _ = ()
