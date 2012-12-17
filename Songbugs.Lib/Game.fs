1b59e4de9ba2575b8ce7e6b1f37be2b1bdb0d159
J. Wostenberg <>
2012-12-17 07:01:35 -0700

    TitleImage should draw something (doesn't though)

--- Songbugs.Lib/Game.fs
+++ Songbugs.Lib/Game.fs
@@ -4,14 +4,18 @@ open Microsoft.Xna.Framework
 type Game() as this =
   inherit Microsoft.Xna.Framework.Game ()
   
+  let mutable title : TitleImage = null
+  
   member val Graphics = new GraphicsDeviceManager(this) with get, set
   
   override this.Initialize () =
     this.Content.RootDirectory <- "../Resources"
     this.Graphics.IsFullScreen <- false
     this.IsMouseVisible <- true
+    title <- new TitleImage(this)
     
     base.Initialize ()
   
-  override this.Draw _ =
+  override this.Draw gameTime =
     this.GraphicsDevice.Clear Color.Black
+    (title :> IGameObject).Draw gameTime
--- Songbugs.Lib/TitleImage.fs
+++ Songbugs.Lib/TitleImage.fs
@@ -1,12 +1,21 @@
 namespace Songbugs.Lib
 open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
 
-type TitleImage () =
+[<AllowNullLiteral>]
+type TitleImage(game : Game) =
+  let spriteBatch = new SpriteBatch(game.GraphicsDevice)
+  let mutable image : Texture2D = null
+  
   interface IGameObject with
     member this.Initialize () = ()
     
-    member this.LoadContent () = ()
+    member this.LoadContent () =
+      image <- game.Content.Load "title.png"
     
     member this.Update _ = ()
     
-    member this.Draw _ = ()
+    member this.Draw gameTime =
+      spriteBatch.Begin ()
+      if not (image = null) then spriteBatch.Draw (image, new Vector2(1.0f, 1.0f), Color.White)
+      spriteBatch.End ()
