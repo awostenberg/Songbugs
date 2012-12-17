e715e24697cccdd95e132638f1ef2ad36ff18d28
J. Wostenberg <>
2012-12-17 07:14:52 -0700

    Fixed media loading from the wrong location

--- Songbugs.Lib/Game.fs
+++ Songbugs.Lib/Game.fs
@@ -9,12 +9,15 @@ type Game() as this =
   member val Graphics = new GraphicsDeviceManager(this) with get, set
   
   override this.Initialize () =
-    this.Content.RootDirectory <- "../Resources"
+    this.Content.RootDirectory <- "../Resources/Media"
     this.Graphics.IsFullScreen <- false
     this.IsMouseVisible <- true
     title <- new TitleImage(this)
     
     base.Initialize ()
+  
+  override this.LoadContent () =
+    title.LoadContent ()
   
   override this.Draw gameTime =
     this.GraphicsDevice.Clear Color.Black
