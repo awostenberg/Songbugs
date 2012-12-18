c468fed8a9ef44c1cbea531e088ba34150d293b1
J. Wostenberg <>
2012-12-17 21:02:28 -0700

    change elements type to a .NET 2D array (its mutable) and added a Draw method that doesn't really do anything interesting yet

--- Songbugs.Lib/Alignment.fs
+++ Songbugs.Lib/Alignment.fs
@@ -2,10 +2,13 @@ namespace Songbugs.Lib
 open Microsoft.Xna.Framework
 
 // Used to order objects on the screen with "slots".
-type Alignment(slots : int * int, elements : int list list) =
+type Alignment(slots : int * int, elements : GameObject [,]) =
   inherit GameObject()
   
-  new(slots) = Alignment(slots, [])
+  new(slots) = Alignment(slots, Array2D.zeroCreate 0 0)
   
   member val Slots = slots with get, set
   member val Elements = elements with get, set
+  
+  override this.Draw gameTime =
+    Array2D.iter (fun (obj : GameObject) -> obj.Draw gameTime) elements
