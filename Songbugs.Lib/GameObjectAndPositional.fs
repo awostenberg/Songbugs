f503949ca8c655ff3c96e63d9e48a04c40c3dbae
J. Wostenberg <>
2012-12-18 07:00:17 -0700

    Created an abstract subclass of GameObject called Positional, and renamed GameObject.fs to GameObjectAndPositional.fs

---
+++ Songbugs.Lib/GameObjectAndPositional.fs
@@ -0,0 +1,23 @@
+namespace Songbugs.Lib
+open Microsoft.Xna.Framework
+
+[<AllowNullLiteral>]
+type GameObject() =
+  abstract Initialize : unit -> unit default this.Initialize () = ()
+  
+  abstract LoadContent : unit -> unit default this.LoadContent () = ()
+  
+  abstract Update : GameTime -> unit default this.Update _ = ()
+  
+  abstract Draw : GameTime -> unit default this.Draw _ = ()
+
+[<AllowNullLiteral>]
+type Positional() =
+  inherit GameObject()
+  
+  let mutable _position = Vector2.Zero
+  
+  abstract Position : Vector2 with get, set
+  override this.Position
+    with get() = _position
+    and set(v) = _position <- v
