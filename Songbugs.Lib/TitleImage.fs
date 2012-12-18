e959ed01b976cf9360b53a27cbbc37886117b84a
J. Wostenberg <>
2012-12-17 20:21:29 -0700

    Got scaling working for the most part

--- Songbugs.Lib/TitleImage.fs
+++ Songbugs.Lib/TitleImage.fs
@@ -1,4 +1,5 @@
 namespace Songbugs.Lib
+open System
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 
@@ -8,11 +9,12 @@ type TitleImage(game : Game) =
   
   let spriteBatch = new SpriteBatch(game.GraphicsDevice)
   let mutable image : Texture2D = null
+  let scale = 1.0f
   
   override this.LoadContent () =
     image <- game.Content.Load "title.png"
   
   override this.Draw _ =
     spriteBatch.Begin ()
-    if not (image = null) then spriteBatch.Draw (image, new Vector2(1.0f, 1.0f), Color.White)
+    if not (image = null) then spriteBatch.Draw (image, Vector2.Zero, new Nullable<Rectangle>(), Color.White, 0.0f, Vector2.Zero, scale, SpriteEffects.None, 0.0f)
     spriteBatch.End ()
