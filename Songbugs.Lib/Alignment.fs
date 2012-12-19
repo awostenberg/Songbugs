073a5e0124380fbfdf04ad19d2eb3d2734898ca8
J. Wostenberg <>
2012-12-19 07:06:54 -0700

    Everything draws in the correct position now

--- Songbugs.Lib/Alignment.fs
+++ Songbugs.Lib/Alignment.fs
@@ -2,10 +2,11 @@ namespace Songbugs.Lib
 open Microsoft.Xna.Framework
 
 // Used to order objects on the screen with "slots"
+// Note to self: Don't use size (little s), but use Size (property)
 type Alignment(slotWidth: int, slotHeight : int, size : Vector2, elements : Positional [,]) =
   inherit Positional()
   
-  let elementsDo action = Array2D.iter action elements
+  let elementsDo action = Array2D.iteri action elements
   
   new(slotWidth, slotHeight, size) = Alignment(slotWidth, slotHeight, size, Array2D.zeroCreate slotWidth slotHeight)
   new(slotWidth, slotHeight) = Alignment(slotWidth, slotHeight, Vector2.Zero)
@@ -21,13 +22,15 @@ type Alignment(slotWidth: int, slotHeight : int, size : Vector2, elements : Posi
   
   member this.Add elem x y = Array2D.set elements x y elem
   
-  override this.Initialize () = elementsDo (fun obj -> obj.Initialize ())
+  override this.Initialize () = elementsDo (fun x y obj -> obj.Initialize ())
   
-  override this.LoadContent () = elementsDo (fun obj -> obj.LoadContent ())
+  override this.LoadContent () = elementsDo (fun x y obj -> obj.LoadContent ())
   
   override this.Update gameTime =
-    elementsDo (fun obj ->
+    elementsDo (fun x y obj ->
       obj.Update gameTime
-      obj.Position <- new Vector2(100.0f, 150.0f))
+      let x = this.Size.X / (slotWidth + 1 |> float32)
+      let y = this.Size.Y / (slotHeight + 1 |> float32)
+      obj.Position <- new Vector2(x, y))
   
-  override this.Draw gameTime = elementsDo (fun obj -> obj.Draw gameTime)
+  override this.Draw gameTime = elementsDo (fun x y obj -> obj.Draw gameTime)
