0162f1a696708a59039e891f9c525db3697190f4
J. Wostenberg <>
2012-12-19 07:43:16 -0700

    Okay, it works right now. Yay

--- Songbugs.Lib/Alignment.fs
+++ Songbugs.Lib/Alignment.fs
@@ -7,6 +7,7 @@ type Alignment(slotWidth: int, slotHeight : int, size : Vector2, elements : Posi
   inherit Positional()
   
   let elementsDo action = Array2D.iteri action elements
+  let safeElementsDo action = elementsDo (fun x y obj -> if not (obj = null) then action x y obj)
   
   new(slotWidth, slotHeight, size) = Alignment(slotWidth, slotHeight, size, Array2D.zeroCreate slotWidth slotHeight)
   new(slotWidth, slotHeight) = Alignment(slotWidth, slotHeight, Vector2.Zero)
@@ -22,15 +23,15 @@ type Alignment(slotWidth: int, slotHeight : int, size : Vector2, elements : Posi
   
   member this.Add elem x y = Array2D.set elements x y elem
   
-  override this.Initialize () = elementsDo (fun x y obj -> obj.Initialize ())
+  override this.Initialize () = safeElementsDo (fun x y obj -> obj.Initialize ())
   
-  override this.LoadContent () = elementsDo (fun x y obj -> obj.LoadContent ())
+  override this.LoadContent () = safeElementsDo (fun x y obj -> obj.LoadContent ())
   
   override this.Update gameTime =
-    elementsDo (fun x y obj ->
+    safeElementsDo (fun x y obj ->
       obj.Update gameTime
-      let x = this.Size.X / (slotWidth + 1 |> float32)
-      let y = this.Size.Y / (slotHeight + 1 |> float32)
+      let x = this.Size.X / (slotWidth + 1 |> float32) * (x + 1 |> float32)
+      let y = this.Size.Y / (slotHeight + 1 |> float32) * (y + 1 |> float32)
       obj.Position <- new Vector2(x, y))
   
-  override this.Draw gameTime = elementsDo (fun x y obj -> obj.Draw gameTime)
+  override this.Draw gameTime = safeElementsDo (fun x y obj -> obj.Draw gameTime)
