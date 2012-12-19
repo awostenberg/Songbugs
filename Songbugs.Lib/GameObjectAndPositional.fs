028079b826fac5adf599fcd05678d00dfcefedd7
J. Wostenberg <>
2012-12-19 07:36:12 -0700

    Added overloads for operators on Vector2s

--- Songbugs.Lib/GameObjectAndPositional.fs
+++ Songbugs.Lib/GameObjectAndPositional.fs
@@ -10,6 +10,21 @@ type GameObject() =
   abstract Update : GameTime -> unit default this.Update _ = ()
   
   abstract Draw : GameTime -> unit default this.Draw _ = ()
+  
+  // Operator overloads
+  // Vector2 * Vector2
+  
+  static member (+) (v1 : Vector2, v2 : Vector2) = new Vector2(v1.X + v2.X, v1.Y + v2.Y)
+  static member (-) (v1 : Vector2, v2 : Vector2) = new Vector2(v1.X - v2.X, v1.Y - v2.Y)
+  static member (*) (**) (v1 : Vector2, v2 : Vector2) = new Vector2(v1.X * v2.X, v1.Y * v2.Y)
+  static member (/) (v1 : Vector2, v2 : Vector2) = new Vector2(v1.X / v2.X, v1.Y / v2.Y)
+  
+  // Vector2 * float32
+  
+  static member (+) (vec : Vector2, num) = new Vector2(vec.X + num, vec.Y + num)
+  static member (-) (vec : Vector2, num) = new Vector2(vec.X - num, vec.Y - num)
+  static member (*) (**) (vec : Vector2, num) = new Vector2(vec.X - num, vec.Y - num)
+  static member (/) (vec : Vector2, num) = new Vector2(vec.X / num, vec.Y / num)
 
 [<AllowNullLiteral>]
 type Positional() =
