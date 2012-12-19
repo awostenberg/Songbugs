e9d2e291ff519505ec211c4d0feb8e58a31b237d
J. Wostenberg <>
2012-12-18 17:54:59 -0700

    Forgot to initialize alignment

--- Songbugs.Lib/Game.fs
+++ Songbugs.Lib/Game.fs
@@ -4,7 +4,7 @@ open Microsoft.Xna.Framework
 type Game() as this =
   inherit Microsoft.Xna.Framework.Game ()
   
-  let alignment = new Alignment(1, 1)
+  let alignment = new Alignment(1, 1, Vector2.Zero)
   
   member val Graphics = new GraphicsDeviceManager(this) with get, set
   
@@ -12,7 +12,9 @@ type Game() as this =
     this.Content.RootDirectory <- "../Resources/Media"
     this.Graphics.IsFullScreen <- false
     this.IsMouseVisible <- true
+    alignment.Size <- new Vector2(this.Graphics.PreferredBackBufferWidth |> float32, this.Graphics.PreferredBackBufferHeight |> float32)
     alignment.Add (new TitleImage(this)) 0 0
+    alignment.Initialize ()
     
     base.Initialize ()
   
