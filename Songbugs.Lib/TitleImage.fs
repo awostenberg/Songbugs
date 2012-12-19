5aad4d2e7eb41de8cf76db4a0a3332af2acb5f6a
J. Wostenberg <>
2012-12-19 07:36:43 -0700

    Added Width, Height, and Size properties

--- Songbugs.Lib/TitleImage.fs
+++ Songbugs.Lib/TitleImage.fs
@@ -11,10 +11,14 @@ type TitleImage(game : Game) =
   let mutable image : Texture2D = null
   let scale = 4.0f
   
+  member this.Width = image.Width
+  member this.Height = image.Height
+  member this.Size = new Vector2(this.Width |> float32, this.Height |> float32) * scale
+  
   override this.LoadContent () =
     image <- game.Content.Load "title.png"
   
   override this.Draw _ =
     spriteBatch.Begin (SpriteSortMode.Deferred, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.None, RasterizerState.CullCounterClockwise)
-    if not (image = null) then spriteBatch.Draw (image, this.Position, new Nullable<Rectangle>(), Color.White, 0.0f, Vector2.Zero, scale, SpriteEffects.None, 0.0f)
+    if not (image = null) then spriteBatch.Draw (image, this.Position - (this.Size / 2.0f), new Nullable<Rectangle>(), Color.White, 0.0f, Vector2.Zero, scale, SpriteEffects.None, 0.0f)
     spriteBatch.End ()
