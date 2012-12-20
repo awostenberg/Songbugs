e94a420cca4c003a8fd6516761f04b066c44526d
J. Wostenberg <>
2012-12-19 20:20:12 -0700

    Updated Button and TileImage for the new Positional

--- Songbugs.Lib/Button.fs
+++ Songbugs.Lib/Button.fs
@@ -1,6 +1,7 @@
 namespace Songbugs.Lib
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
 
 type Button(game : Game) =
   inherit Positional()
@@ -8,12 +9,12 @@ type Button(game : Game) =
   let spriteBatch = new SpriteBatch(game.GraphicsDevice)
   let mutable i_normal : Texture2D = null
   let mutable i_pressed : Texture2D = null
+  let mutable oldState = Mouse.GetState ()
   
   member val Pressed = false with get, set
   member this.Image = if this.Pressed then i_pressed else i_normal
-  member this.Width = this.Image.Width
-  member this.Height = this.Image.Height
-  member this.Size = new Vector2(this.Image.Width |> float32, this.Image.Height |> float32)
+  override this.Width = this.Image.Width
+  override this.Height = this.Image.Height
   
   override this.LoadContent () =
     i_normal <- game.Content.Load "b_normal.png"
@@ -21,5 +22,5 @@ type Button(game : Game) =
   
   override this.Draw _ =
     spriteBatch.Begin ()
-    spriteBatch.Draw (this.Image, this.Position - (this.Size / 2.0f), Color.White)
+    spriteBatch.Draw (this.Image, this.Position, Color.White)
     spriteBatch.End ()
