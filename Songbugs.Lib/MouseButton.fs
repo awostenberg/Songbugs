ae57a51684f5dfc5bfd985ed2a960788d8b3d08d
J. Wostenberg <>
2012-12-21 07:21:10 -0700

    Created a MouseButton enum since XNA doesn't

---
+++ Songbugs.Lib/MouseButton.fs
@@ -0,0 +1,9 @@
+namespace Microsoft.Xna.Framework.Input
+
+// An enumeration for the mouse buttons, since XNA doesn't provide one
+type MouseButtons =
+  | LeftButton = 0
+  | MiddleButton = 1
+  | RightButton = 2
+  | X1 = 3
+  | X2 = 4
