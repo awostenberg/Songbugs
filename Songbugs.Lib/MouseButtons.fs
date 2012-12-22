f17fb11f4fd9b4515b78808d845c7d2e67cd8d13
J. Wostenberg <>
2012-12-21 17:18:40 -0700

    Renamed [X]Button to just [X]

--- Songbugs.Lib/MouseButtons.fs
+++ Songbugs.Lib/MouseButtons.fs
@@ -2,8 +2,8 @@ namespace Songbugs.Lib.Input
 
 // An enumeration for the mouse buttons, since XNA doesn't provide one
 type MouseButtons =
-  | LeftButton = 0
-  | MiddleButton = 1
-  | RightButton = 2
+  | Left = 0
+  | Middle = 1
+  | Right = 2
   | X1 = 3
   | X2 = 4
