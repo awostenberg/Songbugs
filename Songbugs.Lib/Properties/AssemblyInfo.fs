fc28d7d12a0eee6d2d8ad5e6b4715e3381a50b95
J. Wostenberg <>
2012-12-22 10:39:30 -0700

    Changed name and version

--- Songbugs.Lib/Properties/AssemblyInfo.fs
+++ Songbugs.Lib/Properties/AssemblyInfo.fs
@@ -8,12 +8,12 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("")>]
-[<assembly: AssemblyCopyright("alanwostenberg")>]
+[<assembly: AssemblyCopyright("J. Wostenberg")>]
 [<assembly: AssemblyTrademark("")>]
 
 // The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
 
-[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.0.0.0")>]
 
 //[<assembly: AssemblyDelaySign(false)>]
 //[<assembly: AssemblyKeyFile("")>]
