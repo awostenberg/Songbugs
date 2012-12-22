ade6d38b9b7245d1f8723d911730cfa0142e0ffe
J. Wostenberg <>
2012-12-21 21:59:53 -0700

    Added a MouseRelease event and changed MouseEngage to MousePress

--- Songbugs.Lib/EventManager.fs
+++ Songbugs.Lib/EventManager.fs
@@ -7,18 +7,30 @@ module EventManager =
   
   let mutable private oldMouseState = Mouse.GetState ()
   
-  let private mouseEngage = new Event<MouseButtons>()
+  let private mousePress = new Event<MouseButtons>()
+  let private mouseRelease = new Event<MouseButtons>()
   
-  let MouseEngage = mouseEngage.Publish
+  let MouseEngage = mousePress.Publish
+  let MouseRelease = mouseRelease.Publish
   
   let updateMouseEvents () =
     let mouseState = Mouse.GetState ()
+    
     if (mouseState.LeftButton = ButtonState.Pressed) && (oldMouseState.LeftButton = ButtonState.Released) then
-      mouseEngage.Trigger MouseButtons.Left
+      mousePress.Trigger MouseButtons.Left
+    else if (mouseState.LeftButton = ButtonState.Released) && (oldMouseState.LeftButton = ButtonState.Pressed) then
+      mouseRelease.Trigger MouseButtons.Left
+    
     if (mouseState.MiddleButton = ButtonState.Pressed) && (oldMouseState.MiddleButton = ButtonState.Released) then
-      mouseEngage.Trigger MouseButtons.Middle
+      mousePress.Trigger MouseButtons.Middle
+    else if (mouseState.MiddleButton = ButtonState.Released) && (oldMouseState.MiddleButton = ButtonState.Pressed) then
+      mouseRelease.Trigger MouseButtons.Right
+    
     if (mouseState.RightButton = ButtonState.Pressed) && (oldMouseState.RightButton = ButtonState.Released) then
-      mouseEngage.Trigger MouseButtons.Right
+      mousePress.Trigger MouseButtons.Right
+    else if (mouseState.RightButton = ButtonState.Released) && (oldMouseState.RightButton = ButtonState.Pressed) then
+      mouseRelease.Trigger MouseButtons.Right
+    
     oldMouseState <- mouseState
   
   // Keep those events flowing.
