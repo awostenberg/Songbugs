772d6c98377ef4987b1c581f025e6668274929f5
J. Wostenberg <>
2012-12-21 21:35:14 -0700

    Just noticed that there were the events LeftMouseClick, MiddleMouseClick, and RightMouseClick which all had the button as an agument...

--- Songbugs.Lib/EventManager.fs
+++ Songbugs.Lib/EventManager.fs
@@ -7,23 +7,18 @@ module EventManager =
   
   let mutable private oldMouseState = Mouse.GetState ()
   
-  let private mouseMove = new Event<Vector2>()
-  let private leftMouseClick = new Event<MouseButtons>()
-  let private middleMouseClick = new Event<MouseButtons>()
-  let private rightMouseClick = new Event<MouseButtons>()
+  let private MouseClick = new Event<MouseButtons>()
   
-  let LeftMouseClick = leftMouseClick.Publish
-  let MiddleMouseClick = middleMouseClick.Publish
-  let RightMouseClick = rightMouseClick.Publish
+  let MouseClick = MouseClick.Publish
   
   let updateMouseEvents () =
     let mouseState = Mouse.GetState ()
     if (mouseState.LeftButton = ButtonState.Pressed) && (oldMouseState.LeftButton = ButtonState.Released) then
-      leftMouseClick.Trigger MouseButtons.Left
+      MouseClick.Trigger MouseButtons.Left
     if (mouseState.MiddleButton = ButtonState.Pressed) && (oldMouseState.MiddleButton = ButtonState.Released) then
-      leftMouseClick.Trigger MouseButtons.Middle
+      MouseClick.Trigger MouseButtons.Middle
     if (mouseState.RightButton = ButtonState.Pressed) && (oldMouseState.RightButton = ButtonState.Released) then
-      leftMouseClick.Trigger MouseButtons.Right
+      MouseClick.Trigger MouseButtons.Right
   
   // Call this to keep events flowing
   let update () =
