c009da224f1b8f52d9aa775efba8dfa56e8ad516
J. Wostenberg <>
2012-12-22 10:50:55 -0700

    Changed Button to use events

--- Songbugs.Lib/Button.fs
+++ Songbugs.Lib/Button.fs
@@ -9,25 +9,22 @@ type Button(game : Game) =
   let spriteBatch = new SpriteBatch(game.GraphicsDevice)
   let mutable i_normal : Texture2D = null
   let mutable i_pressed : Texture2D = null
-  let mutable oldState = Mouse.GetState ()
   
   member val Pressed = false with get, set
   member this.Image = if this.Pressed then i_pressed else i_normal
   override this.Width = this.Image.Width
   override this.Height = this.Image.Height
   
+  override this.Initialize () =
+    let containsMouse () =
+      let mState = Mouse.GetState ()
+      this.Bounds.Contains (new Vector2(mState.X |> float32, mState.Y |> float32))
+    EventManager.MousePress.Add (fun b -> if containsMouse () then this.Pressed <- true)
+    EventManager.MouseRelease.Add (fun b -> this.Pressed <- false)
+  
   override this.LoadContent () =
     i_normal <- game.Content.Load "b_normal.png"
     i_pressed <- game.Content.Load "b_pressed.png"
-  
-  member this.BeingClicked (state : MouseState) =
-    let p = new Vector2(state.X |> float32, state.Y |> float32)
-    this.Bounds.Contains p && (state.LeftButton = ButtonState.Pressed)
-  
-  override this.Update _ =
-    let state = Mouse.GetState ()
-    this.Pressed <- this.BeingClicked state
-    oldState <- state
   
   override this.Draw _ =
     spriteBatch.Begin ()
