e3e005c2f5534d2438e8d15b14ab9b9814339e9c
J. Wostenberg <>
2012-12-21 17:26:13 -0700

    Added a method to update events

--- Songbugs.Lib/EventManager.fs
+++ Songbugs.Lib/EventManager.fs
@@ -5,6 +5,8 @@ open Songbugs.Lib.Input
 
 module EventManager =
   
+  let mutable private oldMouseState = Mouse.GetState ()
+  
   let private mouseMove = new Event<Vector2>()
   let private leftMouseClick = new Event<MouseButtons>()
   let private middleMouseClick = new Event<MouseButtons>()
@@ -13,3 +15,16 @@ module EventManager =
   let LeftMouseClick = leftMouseClick.Publish
   let MiddleMouseClick = middleMouseClick.Publish
   let RightMouseClick = rightMouseClick.Publish
+  
+  let updateMouseEvents () =
+    let mouseState = Mouse.GetState ()
+    if (mouseState.LeftButton = ButtonState.Pressed) && (oldMouseState.LeftButton = ButtonState.Released) then
+      leftMouseClick.Trigger MouseButtons.Left
+    if (mouseState.MiddleButton = ButtonState.Pressed) && (oldMouseState.MiddleButton = ButtonState.Released) then
+      leftMouseClick.Trigger MouseButtons.Middle
+    if (mouseState.RightButton = ButtonState.Pressed) && (oldMouseState.RightButton = ButtonState.Released) then
+      leftMouseClick.Trigger MouseButtons.Right
+  
+  // Call this to keep events flowing
+  let update () =
+    updateMouseEvents ()
