93777b93de64a3a4a774ec13cf687b3c20183f63
J. Wostenberg <>
2012-12-21 21:37:02 -0700

    Wow fixed some more typos of the same kind

--- Songbugs.Lib/EventManager.fs
+++ Songbugs.Lib/EventManager.fs
@@ -14,11 +14,11 @@ module EventManager =
   let updateMouseEvents () =
     let mouseState = Mouse.GetState ()
     if (mouseState.LeftButton = ButtonState.Pressed) && (oldMouseState.LeftButton = ButtonState.Released) then
-      MouseClick.Trigger MouseButtons.Left
+      mouseClick.Trigger MouseButtons.Left
     if (mouseState.MiddleButton = ButtonState.Pressed) && (oldMouseState.MiddleButton = ButtonState.Released) then
-      MouseClick.Trigger MouseButtons.Middle
+      mouseClick.Trigger MouseButtons.Middle
     if (mouseState.RightButton = ButtonState.Pressed) && (oldMouseState.RightButton = ButtonState.Released) then
-      MouseClick.Trigger MouseButtons.Right
+      mouseClick.Trigger MouseButtons.Right
   
   // Call this to keep events flowing
   let update () =
