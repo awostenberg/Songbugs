18d4d7e72a1ddda5a08622f8d7ef98d665b83fb1
J. Wostenberg <>
2012-12-22 13:10:30 -0700

    Starting to refactor some

--- Songbugs.Lib/EventManager.fs
+++ Songbugs.Lib/EventManager.fs
@@ -18,27 +18,34 @@ module EventManager =
   let updateMouseEvents () =
     let mouseState = Mouse.GetState ()
     
+    let mousePress curr old evArg =
+      if (curr = ButtonState.Pressed) && (old = ButtonState.Released) then
+        mousePress.Trigger evArg
+    
     // All these if statements can somehow be refactored into some function. I'll do that soon; this is ugly...
-    if (mouseState.LeftButton = ButtonState.Pressed) && (oldMouseState.LeftButton = ButtonState.Released) then
-      mousePress.Trigger MouseButtons.Left
-    else if mouseState.LeftButton = ButtonState.Pressed then
-      mouseDown.Trigger MouseButtons.Left
-    else if (mouseState.LeftButton = ButtonState.Released) && (oldMouseState.LeftButton = ButtonState.Pressed) then
-      mouseRelease.Trigger MouseButtons.Left
+    //if (mouseState.LeftButton = ButtonState.Pressed) && (oldMouseState.LeftButton = ButtonState.Released) then
+    //  mousePress.Trigger MouseButtons.Left
+    //else if mouseState.LeftButton = ButtonState.Pressed then
+    //  mouseDown.Trigger MouseButtons.Left
+    //else if (mouseState.LeftButton = ButtonState.Released) && (oldMouseState.LeftButton = ButtonState.Pressed) then
+    //  mouseRelease.Trigger MouseButtons.Left
+    mousePress mouseState.LeftButton oldMouseState.LeftButton MouseButtons.Left
     
-    if (mouseState.MiddleButton = ButtonState.Pressed) && (oldMouseState.MiddleButton = ButtonState.Released) then
-      mousePress.Trigger MouseButtons.Middle
-    else if mouseState.MiddleButton = ButtonState.Pressed then
-      mouseDown.Trigger MouseButtons.Middle
-    else if (mouseState.MiddleButton = ButtonState.Released) && (oldMouseState.MiddleButton = ButtonState.Pressed) then
-      mouseRelease.Trigger MouseButtons.Middle
+    //if (mouseState.MiddleButton = ButtonState.Pressed) && (oldMouseState.MiddleButton = ButtonState.Released) then
+    //  mousePress.Trigger MouseButtons.Middle
+    //else if mouseState.MiddleButton = ButtonState.Pressed then
+    //  mouseDown.Trigger MouseButtons.Middle
+    //else if (mouseState.MiddleButton = ButtonState.Released) && (oldMouseState.MiddleButton = ButtonState.Pressed) then
+    //  mouseRelease.Trigger MouseButtons.Middle
+    mousePress mouseState.MiddleButton oldMouseState.MiddleButton MouseButtons.Middle
     
-    if (mouseState.RightButton = ButtonState.Pressed) && (oldMouseState.RightButton = ButtonState.Released) then
-      mousePress.Trigger MouseButtons.Right
-    else if mouseState.RightButton = ButtonState.Pressed then
-      mouseDown.Trigger MouseButtons.Right
-    else if (mouseState.RightButton = ButtonState.Released) && (oldMouseState.RightButton = ButtonState.Pressed) then
-      mouseRelease.Trigger MouseButtons.Right
+    //if (mouseState.RightButton = ButtonState.Pressed) && (oldMouseState.RightButton = ButtonState.Released) then
+    //  mousePress.Trigger MouseButtons.Right
+    //else if mouseState.RightButton = ButtonState.Pressed then
+    //  mouseDown.Trigger MouseButtons.Right
+    //else if (mouseState.RightButton = ButtonState.Released) && (oldMouseState.RightButton = ButtonState.Pressed) then
+    //  mouseRelease.Trigger MouseButtons.Right
+    mousePress mouseState.RightButton oldMouseState.RightButton MouseButtons.Right
     
     oldMouseState <- mouseState
   
