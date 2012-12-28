0af7e312cb0cee12c75de41a4013320838189b3b
J. Wostenberg <>
2012-12-28 10:53:15 -0700

    Switches to the main game when the button is pressed, but errors out because there is no main game

--- Songbugs.Lib/MainMenu.fs
+++ Songbugs.Lib/MainMenu.fs
@@ -1,5 +1,6 @@
 namespace Songbugs.Lib
 open Microsoft.Xna.Framework
+open MiscOps
 
 [<AllowNullLiteral>]
 type MainMenu(game, size : Vector2) =
@@ -8,8 +9,11 @@ type MainMenu(game, size : Vector2) =
   let alignment = new Alignment(1, 3, Vector2.Zero)
   
   override this.Initialize () =
+    let b = new Button(game)
+    // Since one of these classes has to be defined first, we have to use reflection
+    b.Click.Add (fun () -> setProperty game "CurrentScreen" [|1|] (* 1 is the main game *))
     alignment.[0, 0] <- new TitleImage(game)
-    alignment.[0, 2] <- new Button(game)
+    alignment.[0, 2] <- b
     alignment.Size <- size
     alignment.Initialize ()
   
