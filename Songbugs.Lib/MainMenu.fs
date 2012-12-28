974e9642b0acbfd7cc1523de315e7f4cc7ca010e
J. Wostenberg <>
2012-12-27 19:30:38 -0700

    MainMenu now draws a title image and a button

--- Songbugs.Lib/MainMenu.fs
+++ Songbugs.Lib/MainMenu.fs
@@ -1,4 +1,18 @@
 namespace Songbugs.Lib
+open Microsoft.Xna.Framework
 
-type MainMenu() =
+type MainMenu(game, size : Vector2) =
   inherit GameScreen()
+  
+  let alignment = new Alignment(1, 3, Vector2.Zero)
+  
+  override this.Initialize () =
+    alignment.[0, 0] <- new TitleImage(game)
+    alignment.[0, 2] <- new Button(game)
+    alignment.Size <- size
+  
+  override this.LoadContent () = alignment.LoadContent ()
+  
+  override this.Update gameTime = alignment.Update gameTime
+  
+  override this.Draw gameTime = alignment.Draw gameTime
