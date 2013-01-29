ef830b28c34e6b90734053ff789c598c2c51a21f
J. Wostenberg <>
2013-01-29 07:13:11 -0700

    Switched to a different but better way to draw the palette

--- Songbugs.Lib/GUI/Palette.fs
+++ Songbugs.Lib/GUI/Palette.fs
@@ -7,6 +7,8 @@ type Palette(game : Game) =
   
   let spriteBatch = new SpriteBatch(game.GraphicsDevice)
   let mutable images : Texture2D list = []
+  let drawCenter (img : Texture2D) pos =
+    spriteBatch.Draw (img, pos + new Vector2(img.Width / -2 |> float32, img.Height / -2 |> float32), Color.White)
   
   override this.LoadContent () =
     images <- [game.Content.Load "p_left.png"; game.Content.Load "p_chunk.png"; game.Content.Load "p_right.png"]
@@ -18,9 +20,9 @@ type Palette(game : Game) =
     spriteBatch.Begin ()
     let pp = game.GraphicsDevice.PresentationParameters
     let centerFocus = new Vector2(pp.BackBufferWidth / 2 |> float32, pp.BackBufferHeight |> float32)
-    let i1 = images.[1]
-    let leftBounds, rightBounds = -4.5f, 2.5f
+    let i0, i1, i2 = images.[0], images.[1], images.[2]
+    let leftBounds, rightBounds = -3.5f, 3.5f
+    let yoffset = new Vector2(0.0f, i1.Height / 2 |> float32) * -1.0f
     for xslot in leftBounds..rightBounds do
-      let offset = new Vector2(i1.Width / 2 |> float32, i1.Height * -1 |> float32) + centerFocus
-      spriteBatch.Draw(i1, new Vector2((i1.Width |> float32) * xslot, 0.0f) + offset, Color.White)
+      drawCenter i1 (new Vector2((i1.Width |> float32) * xslot, 0.0f) + yoffset + centerFocus)
     spriteBatch.End ()
