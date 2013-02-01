b11424b56f086c06da86ce20e6fadd09afa589b0
J. Wostenberg <>
2013-01-31 23:43:50 -0700

    Tiles can now draw

--- Songbugs.Lib/GUI/Tile.fs
+++ Songbugs.Lib/GUI/Tile.fs
@@ -2,5 +2,16 @@ namespace Songbugs.Lib
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 
-type Tile (game : Game) =
+type Tile (game : Game, color) =
   inherit Positional()
+  
+  let spriteBatch = new SpriteBatch(game.GraphicsDevice)
+  let mutable texture : Texture2D = null
+  
+  override this.LoadContent () =
+    texture <- game.Content.Load "tile.png"
+  
+  override this.Draw gameTime =
+    spriteBatch.Begin ()
+    spriteBatch.Draw (texture, this.Position, color)
+    spriteBatch.End ()
