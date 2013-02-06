b52685397b0aac3b1eb7f08f427c4f9c36ccab13
J. Wostenberg <>
2013-02-05 21:33:04 -0700

    Added (red for now) tiles to every slot in the pallete

--- Songbugs.Lib/GUI/Palette.fs
+++ Songbugs.Lib/GUI/Palette.fs
@@ -7,14 +7,16 @@ type Palette(game : Game) =
   
   let spriteBatch = new SpriteBatch(game.GraphicsDevice)
   let mutable images : Texture2D list = []
-  let drawCenter (img : Texture2D) pos =
+  let tiles = List.init 8 (fun _ -> new Tile(game, Color.Red))
+  let drawCenter (img : Texture2D) (pos : Vector2) =
     spriteBatch.Draw (img, pos + new Vector2(img.Width / -2 |> float32, img.Height / -2 |> float32), Color.White)
   
   override this.LoadContent () =
     images <- [game.Content.Load "p_left.png"; game.Content.Load "p_chunk.png"; game.Content.Load "p_right.png"]
+    tiles |> List.iter (fun t -> t.LoadContent ())
   
   override this.Update gameTime =
-    ()
+    tiles |> List.iter (fun t -> t.Update gameTime)
   
   override this.Draw gameTime =
     spriteBatch.Begin ()
@@ -25,6 +27,11 @@ type Palette(game : Game) =
     let yoffset = new Vector2(0.0f, i1.Height / 2 |> float32) * -1.0f
     spriteBatch.Draw (i0, new Vector2((i1.Width |> float32) * (leftBounds - 0.5f), 0.0f) + centerFocus + (yoffset * 2.0f) - new Vector2(i0.Width |> float32, 0.0f), Color.White)
     for xslot in leftBounds..rightBounds do
-      drawCenter i1 (new Vector2((i1.Width |> float32) * xslot, 0.0f) + yoffset + centerFocus)
+      let pos = (new Vector2((i1.Width |> float32) * xslot, 0.0f) + yoffset + centerFocus)
+      drawCenter i1 pos
+      let t = tiles.[xslot + 3.5f |> int]
+      t.Position <- pos
+      //t.Draw gameTime
     spriteBatch.Draw (i2, new Vector2((i1.Width |> float32) * (rightBounds + 0.5f), 0.0f) + centerFocus + (yoffset * 2.0f), Color.White)
     spriteBatch.End ()
+    List.iter (fun (t : Tile) -> t.Draw gameTime) tiles
--- Songbugs.Lib/GUI/Tile.fs
+++ Songbugs.Lib/GUI/Tile.fs
@@ -1,6 +1,7 @@
 namespace Songbugs.Lib
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
+open Microsoft.Xna.Framework.Input
 
 type Tile (game : Game, color) =
   inherit Positional()
@@ -8,10 +9,14 @@ type Tile (game : Game, color) =
   let spriteBatch = new SpriteBatch(game.GraphicsDevice)
   let mutable texture : Texture2D = null
   
+  override this.Width = texture.Width
+  override this.Height = texture.Height
+  
   override this.LoadContent () =
     texture <- game.Content.Load "tile.png"
   
   override this.Draw gameTime =
     spriteBatch.Begin ()
-    spriteBatch.Draw (texture, this.Position, color)
+    let mState = Mouse.GetState ()
+    spriteBatch.Draw (texture, this.Position - (this.Size / 2.0f), color)
     spriteBatch.End ()
