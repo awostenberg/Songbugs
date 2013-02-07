2b48eeda4b07b5e06661df78c2b57bb33fa348c6
J. Wostenberg <>
2013-02-06 21:40:09 -0700

    Added the option of creating an empty space in the palette

--- Songbugs.Lib/GUI/Palette.fs
+++ Songbugs.Lib/GUI/Palette.fs
@@ -7,31 +7,35 @@ type Palette(game : Game) =
   
   let spriteBatch = new SpriteBatch(game.GraphicsDevice)
   let mutable images : Texture2D list = []
-  //let tiles = List.init 8 (fun _ -> new Tile(game, "e4"))
-  let tiles = ["rest"; "c4"; "d4"; "e4"; "f4"; "g4"; "a4"; "b4"] |> List.map (fun note -> new Tile(game, note))
+  let tiles = ["rest"; ""; "c4"; "d4"; "e4"; "f4"; "g4"; "a4"; "b4"] |> List.map (fun note ->
+    match note with
+    | "" -> None
+    | _ -> Some (new Tile (game, note)))
   let drawCenter (img : Texture2D) (pos : Vector2) =
     spriteBatch.Draw (img, pos + new Vector2(img.Width / -2 |> float32, img.Height / -2 |> float32), Color.White)
+  // Only execute the action with the tile if it is not a None value
+  let someTileDo action t =
+    if not (t = None) then action (Option.get t)
   
   override this.LoadContent () =
     images <- [game.Content.Load "p_left.png"; game.Content.Load "p_chunk.png"; game.Content.Load "p_right.png"]
-    tiles |> List.iter (fun t -> t.LoadContent ())
+    tiles |> List.iter (fun t -> (someTileDo (fun (t : Tile) -> t.LoadContent ()) t))
   
   override this.Update gameTime =
-    tiles |> List.iter (fun t -> t.Update gameTime)
+    tiles |> List.iter (fun t -> (someTileDo (fun (t : Tile) -> t.Update gameTime) t))
   
   override this.Draw gameTime =
     spriteBatch.Begin ()
     let pp = game.GraphicsDevice.PresentationParameters
     let centerFocus = new Vector2(pp.BackBufferWidth / 2 |> float32, pp.BackBufferHeight |> float32)
     let i0, i1, i2 = images.[0], images.[1], images.[2]
-    let leftBounds, rightBounds = -3.5f, 3.5f
+    let leftBounds, rightBounds = -4.0f, 4.0f
     let yoffset = new Vector2(0.0f, i1.Height / 2 |> float32) * -1.0f
     spriteBatch.Draw (i0, new Vector2((i1.Width |> float32) * (leftBounds - 0.5f), 0.0f) + centerFocus + (yoffset * 2.0f) - new Vector2(i0.Width |> float32, 0.0f), Color.White)
     for xslot in leftBounds..rightBounds do
       let pos = (new Vector2((i1.Width |> float32) * xslot, 0.0f) + yoffset + centerFocus)
       drawCenter i1 pos
-      let t = tiles.[xslot + 3.5f |> int]
-      t.Position <- pos
+      tiles.[xslot + 4.0f |> int] |> someTileDo (fun (t : Tile) -> t.Position <- pos)
     spriteBatch.Draw (i2, new Vector2((i1.Width |> float32) * (rightBounds + 0.5f), 0.0f) + centerFocus + (yoffset * 2.0f), Color.White)
     spriteBatch.End ()
-    List.iter (fun (t : Tile) -> t.Draw gameTime) tiles
+    List.iter (fun t -> someTileDo (fun (t : Tile) -> t.Draw gameTime) t) tiles
